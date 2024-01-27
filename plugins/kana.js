618b297bb083ba2a22201bcb5ae271677d85c07f
johan-hadini <156672332+johan-hadini@users.noreply.github.com>
2024-01-27 13:36:18 +0100

    Update kana.js

--- plugins/kana.js
+++ plugins/kana.js
@@ -3,9 +3,9 @@ const handler = (m) => m;
 handler.all = async function(m) {
   const vn = './media/zoro.mp3';
   const chat = global.db.data.chats[m.chat];
-  if (/^كانا$/i.test(m.text) && !chat.isBanned) {
+  if (/^لوفي$/i.test(m.text) && !chat.isBanned) {
     conn.sendPresenceUpdate('recording', m.chat);
-    await m.reply(`*اهلا, ¿انا كانا?*`);
+    await m.reply(`*اهلا, ¿انا لوفي?*`);
     m.conn.sendMessage(m.chat, {audio: {url: vn}, fileName: 'error.mp3', mimetype: 'audio/mpeg', ptt: true}, {quoted: m});
   }
   return !0;
