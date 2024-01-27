9d54f2bb493735ba1e814d361b8b20ea5cda418d
johan-hadini <156672332+johan-hadini@users.noreply.github.com>
2024-01-27 19:15:43 +0100

    Update antiprivate.js

--- plugins/antiprivate.js
+++ plugins/antiprivate.js
@@ -1,1 +1,13 @@
-johan 3mk
+export async function before(m, {conn, isAdmin, isBotAdmin, isOwner, isROwner}) {
+  if (m.isBaileys && m.fromMe) return !0;
+  if (m.isGroup) return !1;
+  if (!m.message) return !0;
+  if (m.text.includes('PIEDRA') || m.text.includes('PAPEL') || m.text.includes('TIJERA') || m.text.includes('serbot') || m.text.includes('jadibot')) return !0;
+  const chat = global.db.data.chats[m.chat];
+  const bot = global.db.data.settings[this.user.jid] || {};
+  if (bot.antiPrivate && !isOwner && !isROwner) {
+    await m.reply(`*[❗] ʜᴏʟᴀ @${m.sender.split`@`[0]}, كلم المطور لرفع لحظر عنكك او ادخل جروب دعم رسمي للبوت المطور: +22237979043 يمنع التحدث بالرسائل الخاصة للبوت وبالتالي سيتم حظرك.*`, false, {mentions: [m.sender]});
+    await this.updateBlockStatus(m.chat, 'block');
+  }
+  return !1;
+}
