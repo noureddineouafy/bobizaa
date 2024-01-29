d20fee991352b566e1187783f928bbbf98647323
johan-hadini <156672332+johan-hadini@users.noreply.github.com>
2024-01-29 02:34:40 +0100

    Update menus-menu.js

--- plugins/menus-menu.js
+++ plugins/menus-menu.js
@@ -172,7 +172,7 @@ conn.sendMessage(m.chat, {
 }; 
 handler.help = ['main']
 handler.tags = ['group']
-handler.command = ['اوامر'] 
+handler.command = ['المهام'] 
 
 export default handler
 function clockString(ms) {
