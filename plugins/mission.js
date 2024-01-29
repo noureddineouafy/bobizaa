e1d58509103fa61669c3a9521678d15c43816868
johan-hadini <156672332+johan-hadini@users.noreply.github.com>
2024-01-29 03:20:20 +0100

    Update mission.js

--- plugins/mission.js
+++ plugins/mission.js
@@ -1,37 +1,87 @@
-let handler = async (m, { conn }) => {
-    
-    const name = conn.getName(m.sender);
-    let videoUrl = 'https://telegra.ph/file/3730115217e34afa7f21a.mp4';
-    let tagUser = '@' + m.sender.split("@s.whatsapp.net")[0];
-    let more = String.fromCharCode(8206);
-    let teks = `${pickRandom([`*◞♥️بِسۡمِ ٱللَّهِ ٱلرَّحۡمَٰنِ ٱلرَّحِيمِ♥️◜*
- *↻╍╍━┛🕋┗━╍╍↺*
-*◞{إِنَّمَا ٱلتَّوۡبَةُ عَلَى ٱللَّهِ لِلَّذِينَ يَعۡمَلُونَ ٱلسُّوٓءَ بِجَهَٰلَةٖ ثُمَّ يَتُوبُونَ مِن قَرِيبٖ فَأُوْلَٰٓئِكَ يَتُوبُ ٱللَّهُ عَلَيۡهِمۡۗ وَكَانَ ٱللَّهُ عَلِيمًا حَكِيمٗا}◜*
-*↻╍╍━┛🕋┗━╍╍↺*
-*◞❐ إسـم الـبـوت ⇠『LUFFY』
-*◞❐ إسـم الـمـنـصـة ⇠『HEROKU』◈◜*
-*◞❐ الـمـطـور ⇠『JOHAN』
-*◞❐ عـدد الـمـسـتـخـدمـيـن ${retotalreg}
-*◞❐ وقـت الـتـشـغـيـل ${uptime}
-*◞❐ الـتـوقـيـت ${date}
-*◞❐ ـطـلـب قـائـمـة الـأوامـر إكـتـب ⇠『المهام』
-*◞❐ مـلاحـظـة 🛑 ◈◜*
-*◞❐ يـجـب عـلـيـك كـتـابـة نـقـطـة قـبـل كـل أمـر مـثـل ⇠ 『 .تـخـمـيـن 』◈◜*
-`])}`.trim();
+import { createHash } from 'crypto'
+import { canLevelUp, xpRange } from '../lib/levelling.js'
+import fetch from 'node-fetch'
+import fs from 'fs'
+const { levelling } = '../lib/levelling.js'
+import moment from 'moment-timezone'
+import { promises } from 'fs'
+import { join } from 'path'
+const time = moment.tz('Egypt').format('HH')
+let wib = moment.tz('Egypt').format('HH:mm:ss')
+//import db from '../lib/database.js'
 
-    conn.sendMessage(m.chat, {
-        video: { url: videoUrl },
-        caption: teks,
-        mimetype: 'video/mp4',
-        filename: 'video.mp4'
+let handler = async (m, {conn, usedPrefix, usedPrefix: _p, __dirname, text, isPrems}) => {
+    let d = new Date(new Date + 3600000)
+    let locale = 'ar'
+    let week = d.toLocaleDateString(locale, { weekday: 'long' })
+    let date = d.toLocaleDateString(locale, { day: 'numeric', month: 'long', year: 'numeric' })
+    let _uptime = process.uptime() * 1000
+    let uptime = clockString(_uptime)
+let who = m.quoted ? m.quoted.sender : m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
+if (!(who in global.db.data.users)) throw `✳️ The user is not found in my database`
+let videoUrl = 'https://telegra.ph/file/6c32519269d07f4c15192.mp4';
+  let vn = './media/menu.mp3';
+  const user = global.db.data.users[m.sender];
+  const {money, joincount} = global.db.data.users[m.sender];
+  const {exp, limit, level, role} = 
+    global.db.data.users[m.sender];
+let { min, xp, max } = xpRange(user.level, global.multiplier)
+let username = conn.getName(who)
+let math = max - xp
+let sn = createHash('md5').update(who).digest('hex')
+let totalreg = Object.keys(global.db.data.users).length;
+let rtotalreg = Object.values(global.db.data.users).filter(user => user.registered == true).length 
+let more = String.fromCharCode(8206)
+let readMore = more.repeat(900) 
+  const taguser = '@' +  m.sender.split('@s.whatsapp.net')[0];
+let str = ` 
+✧⏤✧⏤ *حالة* ⏤✧⏤✧⏤✧
+🦾 اهلا,* ${taguser}
+⩥ الوقت${uptime}
+⩥ التاريخ${date}
+⩥ المستخدمين${rtotalreg}
+⩥ المنصه ⩾ HEROKU
+✧⏤✧⏤✧⏤✧⏤✧⏤✧⏤✧⏤✧
+⇟ *معلومات المطور* ⇟
+𐎄   🤿‏محمد 𓂄*اسم المطور*𓂁
+
+𐎄 𓂄 JOHAN ⚰𓂁*لقب المطور* 
+
+𐎄  𓂄 https://wa.me/+22237979043 ‬‏𓂁*رقم المطور*
+
+𐎄𝐷𝐸𝑉 b𝐼𝑂 𓂄الضمير صوت هادىء، يخبرك بأن أحداً ينظر إليك. ليس العيب في أن نسقط، ولكن العيب أن لا تستطيع النهوض. خير للإنسان أن يندم على ما فعل من أن يتحسّر على ما لم يفعل. ليس من الصعب أن تضحّي من أجل صديق، ولكن من الصعب أن تجد الصديق الذي يستحق التضحية. مفتاح الفشل هو محاولة إرضاء كل شخص تعرفه𓂁‬`.trim();
+
+conn.sendMessage(m.chat, {
+        video: { url: videoUrl }, caption: str,
+  mentions: [m.sender,global.conn.user.jid],
+  gifPlayback: true,gifAttribution: 0
     }, { quoted: m });
-};
+}; 
+handler.help = ['main']
+handler.tags = ['group']
+handler.command = ['المطور'] 
 
-handler.command = ['list','ليست','الاوامر','اوامر'];
+export default handler
+function clockString(ms) {
+    let h = isNaN(ms) ? '--' : Math.floor(ms / 3600000)
+    let m = isNaN(ms) ? '--' : Math.floor(ms / 60000) % 60
+    let s = isNaN(ms) ? '--' : Math.floor(ms / 1000) % 60
+    return [h, m, s].map(v => v.toString().padStart(2, 0)).join(':')}
 
-
-export default handler;
-
-function pickRandom(list) {
-    return list[Math.floor(Math.random() * list.length)];
-}
+    function ucapan() {
+      const time = moment.tz('Egypt').format('HH')
+      let res = "بداية يوم سعيده ☀️"
+      if (time >= 4) {
+        res = "صباح الخير 🌄"
+      }
+      if (time >= 10) {
+        res = "مساء الخير ☀️"
+      }
+      if (time >= 15) {
+        res = "مساء الخير 🌇"
+      }
+      if (time >= 18) {
+        res = "مساء الخير 🌙"
+      }
+      return res
+      }
