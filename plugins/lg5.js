81d86b333d6a2af22cfd39c4c3baf919b33e37c5
MATRIX-Md <156081145+MATRIX-Md@users.noreply.github.com>
2024-01-30 20:59:11 +0100

    Update lg5.js

--- plugins/lg5.js
+++ plugins/lg5.js
@@ -1,10 +1,10 @@
 import  fetch from 'node-fetch'
 let handler = async (m, { conn, args }) => {
   let response = args.join(' ').split('|')
-  if (!args[0]) throw 'مثال :\n*.lg5* bobiza'
+  if (!args[0]) throw 'مثال :\n*.lg5* matrix'
   m.reply('_إنتظر..._')
   let res = `https://flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=water-logo&script=water-logo&fontsize=90&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&fillTextColor=%23000&shadowGlowColor=%23000&backgroundColor=%23000&text=${response[0]}`
-  conn.sendFile(m.chat, res, 'bobiza.jpg', `instagram.com/noureddine_ouafy`, m, false)
+  conn.sendFile(m.chat, res, 'matrix.jpg', `instagram.com/abd_ilah__35`, m, false)
 }
 handler.help = ['flaming5'].map(v => v + ' <text>')
 handler.tags = ['maker','logo']
