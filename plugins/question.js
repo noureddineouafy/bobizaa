e4397ae2b8884729a5dba6efd0ef32d9878f508e
johan-hadini <156672332+johan-hadini@users.noreply.github.com>
2024-01-30 15:20:48 +0100

    Update question.js

--- plugins/question.js
+++ plugins/question.js
@@ -5,7 +5,7 @@ const handler = async (m, {conn, usedPrefix}) => {
   conn.tekateki = conn.tekateki ? conn.tekateki : {};
   const id = m.chat;
   if (id in conn.tekateki) {
-    conn.reply(m.chat, 'Todavía hay acertijos sin responder en este chat', conn.tekateki[id][0]);
+    conn.reply(m.chat, 'لا تزال هناك سؤال لم تتم الإجابة عليها في هذه الدردشة', conn.tekateki[id][0]);
     throw false;
   }
   const tekateki = JSON.parse(fs.readFileSync(`./src/game/acertijo.json`));
@@ -14,18 +14,18 @@ const handler = async (m, {conn, usedPrefix}) => {
   const clue = _clue.replace(/[A-Za-z]/g, '_');
   const caption = `
 ⷮ *${json.question}*
-*• Tiempo:* ${(timeout / 1000).toFixed(2)} segundos
-*• Bono:* +${poin} Exp
+*• وقت:* ${(timeout / 1000).toFixed(2)} ثواني
+*• الجوائز:* +${poin} Exp
 `.trim();
   conn.tekateki[id] = [
     await conn.reply(m.chat, caption, m), json,
     poin,
     setTimeout(async () => {
-      if (conn.tekateki[id]) await conn.reply(m.chat, `Se acabó el tiempo!\n*Respuesta:* ${json.response}`, conn.tekateki[id][0]);
+      if (conn.tekateki[id]) await conn.reply(m.chat, `انتهى الوقت!\n*إجابة:* ${json.response}`, conn.tekateki[id][0]);
       delete conn.tekateki[id];
     }, timeout)];
 };
 handler.help = ['acertijo'];
 handler.tags = ['game'];
-handler.command = /^(acertijo|acert|pregunta|adivinanza|tekateki)$/i;
+handler.command = /^(سؤال|acert|pregunta|adivinanza|tekateki)$/i;
 export default handler;
