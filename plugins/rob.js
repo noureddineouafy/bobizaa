3d2af9e142392853e2658fb156123babd4dac427
johan-hadini <156672332+johan-hadini@users.noreply.github.com>
2024-01-30 02:23:53 +0100

    Update rob.js

--- plugins/rob.js
+++ plugins/rob.js
@@ -2,27 +2,27 @@
 let ro = 3000
 let handler = async (m, { conn, usedPrefix, command}) => {
     let time = global.db.data.users[m.sender].lastrob + 7200000
-    if (new Date - global.db.data.users[m.sender].lastrob < 7200000) throw `⏱️¡Hey! wait *${msToTime(time - new Date())}* to steal again`
+    if (new Date - global.db.data.users[m.sender].lastrob < 7200000) throw `⏱️¡مرحبًا! انتظر *${msToTime(time - new Date())}* للسرقة مرة أخرى`
     let who
     if (m.isGroup) who = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted ? m.quoted.sender : false
     else who = m.chat
-    if (!who) throw `✳️ Tag someone to steal`
+    if (!who) throw `🔪 ضع علامة على شخص ما للسرقة`
     if (!(who in global.db.data.users)) throw `✳️ The user is not found in my database`
     let users = global.db.data.users[who]
     let rob = Math.floor(Math.random() * ro)
-    if (users.exp < rob) return m.reply(`🔖 @${who.split`@`[0]} has less than *${ro} xp*\nDon't steal from a rotten":`, null, { mentions: [who] })    
+    if (users.exp < rob) return m.reply(`🔖 @${who.split`@`[0]} لديه أقل من *${ro} xp*\nلا تسرق من فاسد":`, null, { mentions: [who] })    
    global.db.data.users[m.sender].exp += rob
    global.db.data.users[who].exp -= rob
   
     m.reply(`
-  ‣ Robbed *${rob} XP* a @${who.split`@`[0]}
+  ‣ مسروق *${rob} XP* a @${who.split`@`[0]}
   `, null, { mentions: [who] })
     global.db.data.users[m.sender].lastrob = new Date * 1
   }
 
   handler.help = ['rob']
   handler.tags = ['economy']
-  handler.command = ['robar', 'rob']
+  handler.command = ['robar', 'هجوم']
   
   export default handler
   
