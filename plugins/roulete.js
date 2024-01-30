886b9bc0c80757b996a063273f262869a817852d
johan-hadini <156672332+johan-hadini@users.noreply.github.com>
2024-01-30 02:33:34 +0100

    Update roulete.js

--- plugins/roulete.js
+++ plugins/roulete.js
@@ -6,27 +6,27 @@ let handler = async (m, { conn, args, text, usedPrefix , command }) => {
 
     let amount = parseInt(args[0])
     let color = args[1]?.toLowerCase()
-    if (args.length < 2 )  throw `✳️ Command Usage: ${usedPrefix + command} <amount> <color>\n\n Example: ${usedPrefix + command} 500 red`
+    if (args.length < 2 )  throw `🎪 استخدام الأمر: ${usedPrefix + Command} <amount> <color>\n\n مثال: ${usedPrefix + Command} 500 أحمر`
     let colores = ['red', 'black']
     let colour = colores[Math.floor(Math.random() * colores.length)];
     let user = global.db.data.users[m.sender]
-    if (isNaN(amount) || amount < 500) throw `✳️ The minimum bet is 500 gold`
-    if (!colores.includes(color)) throw '✳️ You must specify a valid color: red or black'
-    if (user.credit < amount) throw '✳️ You do not have enough gold!'
-    if (amount > 100000) throw `🟥 *You can't bet gold more than 100000*`
+    if (isNaN(amount) || amount < 500) throw `🎰 الحد الأدنى للرهان هو 500 ذهب`
+    if (!colores.includes(color)) throw '😑 يجب تحديد لون صالح: أحمر أو أسود'
+    if (user.credit < amount) throw '😶 ليس لديك ما يكفي من الذهب!'
+    if (amount > 100000) throw `🟥 *لا يمكنك المراهنة بالذهب بأكثر من 100000*`
    let result = ''
     if (colour == color) {
-        result = `${colour == 'red' ? 'The ball landed on 🔴' : 'The ball landed on ⚫'} \n\nYou won ${amount * 2} gold`
+        result = `${colour == 'red' ? 'هبطت الكرة على 🔴' : 'هبطت الكرة على ⚫'} \n\nلقد فزت بذهبية قدرها ${amount * 2}.`
         user.credit += amount * 2
     } else {
-        result = `${colour == 'red' ? 'The ball landed on 🔴' : 'The ball landed on ⚫'} \n\nYou lost ${amount} gold`
+        result = `${colour == 'red' ? 'هبطت الكرة على 🔴' : 'هبطت الكرة على ⚫'} \n\nلقد خسرت ${amount} من الذهب`
         user.credit -= amount
     }
-    m.reply(result)
+    m.reply(نتيجة)
 }
 handler.help = ['roulette <amount> <color(red/black)>']
 handler.tags = ['economy']
-handler.command = ['roulette', 'rt']
+handler.command = ['الروليت', 'rt']
 
 handler.group = true
 
