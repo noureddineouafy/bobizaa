a0a64e6a5e0c9ba2c0e21b3bfb92b91e95e230e6
johan-hadini <156672332+johan-hadini@users.noreply.github.com>
2024-01-30 15:23:53 +0100

    Update questionhal.js

--- plugins/questionhal.js
+++ plugins/questionhal.js
@@ -5,16 +5,16 @@ handler.before = async function(m) {
   const id = m.chat;
   if (!m.quoted || !m.quoted.fromMe || !m.quoted.isBaileys || !/^ⷮ/i.test(m.quoted.text)) return !0;
   this.tekateki = this.tekateki ? this.tekateki : {};
-  if (!(id in this.tekateki)) return m.reply('Ese acertijo ya ha terminado!');
+  if (!(id in this.tekateki)) return m.reply('لقد انتهى هذا سؤال الآن!');
   if (m.quoted.id == this.tekateki[id][0].id) {
     const json = JSON.parse(JSON.stringify(this.tekateki[id][1]));
     if (m.text.toLowerCase() == json.response.toLowerCase().trim()) {
       global.db.data.users[m.sender].exp += this.tekateki[id][2];
-      m.reply(`*Respuesta correcta!*\n+${this.tekateki[id][2]} Exp`);
+      m.reply(`*اجابة صحيحة!*\n+${this.tekateki[id][2]} Exp`);
       clearTimeout(this.tekateki[id][3]);
       delete this.tekateki[id];
-    } else if (similarity(m.text.toLowerCase(), json.response.toLowerCase().trim()) >= threshold) m.reply(`Casi lo logras!`);
-    else m.reply('Respuesta incorrecta!');
+    } else if (similarity(m.text.toLowerCase(), json.response.toLowerCase().trim()) >= threshold) m.reply(`اقتربت من الاجابة!`);
+    else m.reply('إجابة خاطئة!');
   }
   return !0;
 };
