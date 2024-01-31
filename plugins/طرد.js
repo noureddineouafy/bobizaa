39b58970d5ead658ec01fbe57ce3b9db0128bc6f
johan-hadini <156672332+johan-hadini@users.noreply.github.com>
2024-02-01 00:18:02 +0100

    Update ⁨طرد.js⁩

--- plugins/طرد.js
+++ plugins/طرد.js
@@ -7,7 +7,7 @@ if (!m.mentionedJid[0] && !m.quoted) return m.reply(kickte, m.chat, { mentions: 
 let user = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted.sender
 let owr = m.chat.split`-`[0]
 await conn.groupParticipantsUpdate(m.chat, [user], 'remove')
-m.reply(`*تـــم الــطرد !*`) 
+m.reply(`*طردك فحلك روح ابكي عند امك ترجعك 😲!*`) 
 
 }
 
