ce85ce3415346f920fe4cfff0905e7f0d8b801e3
johan-hadini <156672332+johan-hadini@users.noreply.github.com>
2024-02-01 00:32:41 +0100

    Update ⁨لفل.js⁩

--- plugins/لفل.js
+++ plugins/لفل.js
@@ -13,7 +13,7 @@ let user = global.db.data.users[m.sender]
 ┇ *🚒 الفل :* *${user.level}*
 ┇ *♟️ مصنف :* ${role}
 ┇ *♨️ نقاط الخبرة :* *${user.exp - min}/${xp}*
-┛━━━⊰ بـــــــوت ⊱━━━┗
+┛━━━⊰ LUFFY ⊱━━━┗
 
 *تحتاج ${max - user.exp} من نقاط الخبرة للوصول الي مستوي جديد*`
 conn.sendFile(m.chat, pp, 'levelup.jpg', lvl, m)
@@ -27,7 +27,7 @@ conn.sendFile(m.chat, pp, 'levelup.jpg', lvl, m)
 ┇♨️ *المستوي السابق :* *${before}*
 ┇🎉 *المستوي الحالي :* *${user.level}*
 ┇♟️ *التصنيف :* ${role} 
-┛━━━⊰ بـــــوت ⊱━━━┗`.trim()
+┛━━━⊰ LUFFY ⊱━━━┗`.trim()
         try {
             const img = await levelup(teks, user.level)
             conn.sendFile(m.chat, pp, 'levelup.jpg', str, m)
