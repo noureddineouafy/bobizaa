f53c9a5260b45569289e47e8b05877f500221179
johan-hadini <156672332+johan-hadini@users.noreply.github.com>
2024-02-01 10:23:35 +0000

    Update ⁨احزر.js⁩

--- plugins/احزر.js
+++ plugins/احزر.js
@@ -13,7 +13,7 @@ let handler = async (m, { conn, command, usedPrefix }) => {
   ❐↞┇الـوقـت⏳↞ *${(timeout / 1000).toFixed(2)} ┇
   *استخدم .انسحب للأنسحاب*
   ❐↞┇الـجـائـزة💰↞ ${poin} نقاط┇
-🍖LUFFY-BOT🍖
+🌺Nezuko-BOT🌺
      `.trim()
     conn.tebakbendera[id] = [
         await conn.sendFile(m.chat, json.img, '', caption, m),
