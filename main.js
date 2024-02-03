62b734dd87b4e9c48078158cad216c63488bcbaf
johan-hadini <156672332+johan-hadini@users.noreply.github.com>
2024-02-03 16:04:16 +0000

    Update main.js

--- main.js
+++ main.js
@@ -490,7 +490,7 @@ setInterval(async () => {
   if (stopped === 'close' || !conn || !conn.user) return;
   const _uptime = process.uptime() * 1000;
   const uptime = clockString(_uptime);
-  const bio = `LUFFY-BOT [ ⏳ ] Uptime: ${uptime}`;
+  const bio = `𝐵𝛩𝑇-𝑁𝐸𝑍𝑈𝐾𝛩 [ ⏳ ] Uptime: ${uptime}`;
   await conn.updateProfileStatus(bio).catch((_) => _);
 }, 60000);
 function clockString(ms) {
