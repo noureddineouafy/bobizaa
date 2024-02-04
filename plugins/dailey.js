98bac8eee050399d6faa37bfd92e711cd59b4c53
johan-hadini <156672332+johan-hadini@users.noreply.github.com>
2024-02-04 16:57:30 +0000

    Update dailey.js

--- plugins/dailey.js
+++ plugins/dailey.js
@@ -1,7 +1,7 @@
 let handler = async (m, {command, text, conn, usedPrefix}) => {
 
 let url = 'https://api.whatsapp.com/send?phone=';
-let a7a = url + `{text}`;
+let a7a = url + text;
 await conn.reply(m.chat, a7a, m);
 }
     handler.help = ['تبنيد'];
