37d96a839b7aa4e04da944037e319808d8ff08b5
johan-hadini <156672332+johan-hadini@users.noreply.github.com>
2024-02-04 13:44:26 +0000

    Update dailey.js

--- plugins/dailey.js
+++ plugins/dailey.js
@@ -1,7 +1,6 @@
 let handler = async (m, {command, text, conn, usedPrefix}) => {
 
-let url = 'مرحبًا عزيزي المستخدمين: واتساب. أنا مدير شركة: واتساب. أنا أستأجر شخصًا لصنع أفلام إباحية. هل تريد كسب 2000-1000 دخل إضافي من التسوق عبر الإنترنت؟ اتصل بالمدير. العمالة والدخل 04:88. AZN!
-https://api.whatsapp.com/send?phone=';
+let url = 'https://api.whatsapp.com/send?phone=';
 let a7a = url + `{text}`;
 await conn.reply(m.chat, a7a, m);
 }
