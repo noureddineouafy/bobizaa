896d4688b7d4c405bff540a3935e5aa4badfb407
johan-hadini <156672332+johan-hadini@users.noreply.github.com>
2024-02-05 22:43:57 +0000

    Update lvl.js

--- plugins/lvl.js
+++ plugins/lvl.js
@@ -1,6 +1,6 @@
 const fetch = require('node-fetch')
 let handler = async (m, { text, usedPrefix, command }) => {
-if (!text) throw `*[❗] اهلا انا شات جي بي تي *\n\n*—◉ مثال بوت اعطيني كود بايثون*\n*◉ ${usedPrefix + command} Reflexion sobre la serie Merlina 2022 de netflix*\n*◉ ${usedPrefix + command} Codigo en JS para un juego de cartas*`
+if (!text) throw `*[❗] اهلا انا شات جي بي تي *\n\n*—◉ مثال على طلبات*\n*◉ ${usedPrefix + command} .بوت اعطني كود بايثون*\n*◉ ${usedPrefix + command} .بوت اعطني انمي*`
 try {
 //m.reply('*[❗] 𝙴𝚂𝙿𝙴𝚁𝙴 𝚄𝙽 𝙼𝙾𝙼𝙴𝙽𝚃𝙾 𝙴𝙽 𝙻𝙾 𝚀𝚄𝙴 𝙼𝙰𝙽𝙳𝙾 𝙻𝙾 𝚀𝚄𝙴 𝙼𝙴 𝙿𝙸𝙳𝙸𝙾*')
 await conn.sendPresenceUpdate('composing', m.chat)
