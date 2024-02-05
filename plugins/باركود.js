d09e543285301d25bf97d18e8938e621fc65a2eb
johan-hadini <156672332+johan-hadini@users.noreply.github.com>
2024-02-05 23:01:14 +0000

    Update ⁨باركود.js⁩

--- plugins/باركود.js
+++ plugins/باركود.js
@@ -1,6 +1,6 @@
 import { toDataURL } from 'qrcode'
 let handler = async (m, { text, conn }) => {
-if (!text) throw `*حط الحاجه الي عايز تحولها كود*`
+if (!text) throw `*اكتب كلام الي تبغاه يحول الى باركود 🧸*`
 conn.sendFile(m.chat, await toDataURL(text.slice(0, 2048), { scale: 8 }), 'qrcode.png', '¯\\_(BY:HODA_AND_MAHDI', m)
 }
 handler.help = ['', 'code'].map(v => 'qr' + v + ' <teks>')
