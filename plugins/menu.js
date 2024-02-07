8ccd9c650050951921e3251eb7b6c2a379cf5474
Mouads760 <159349348+Mouads760@users.noreply.github.com>
2024-02-07 22:38:11 +0100

    Update menu.js

--- plugins/menu.js
+++ plugins/menu.js
@@ -136,10 +136,10 @@ let handler = async (m, { conn, usedPrefix: _p, __dirname }) => {
 text: text,
 contextInfo: {
 externalAdReply: {
-title: 'BOBIZA BOT ♥',
+title: 'stiven BOT ♥',
 body: "أول بوت واتساب في العالم العربي 💖",
 thumbnailUrl: 'https://telegra.ph/file/2829c7653514416d207e2.jpg',
-sourceUrl: 'https://instagram.com/noureddine_ouafy',
+sourceUrl: 'https://instagram.com/Stiven._.xxl',
 mediaType: 1,
 renderLargerThumbnail: true
 }}}, { quoted: m})
