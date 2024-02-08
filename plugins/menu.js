55b5a0b4aae79e82e7fabcc41a167763a1864f21
Mouads760 <159349348+Mouads760@users.noreply.github.com>
2024-02-08 17:25:50 +0100

    Update menu.js

--- plugins/menu.js
+++ plugins/menu.js
@@ -138,7 +138,7 @@ contextInfo: {
 externalAdReply: {
 title: 'stiven BOT 😺🪐',
 body: "the first Moroccan bot in the world 😺",
-thumbnailUrl: 'https://telegra.ph/file/bd107b2476838b654441e.jpg',
+thumbnailUrl: 'https://telegra.ph/Stiven-02-08',
 sourceUrl: 'https://instagram.com/Stiven._.xxl',
 mediaType: 1,
 renderLargerThumbnail: true
