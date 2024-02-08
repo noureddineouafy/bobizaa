126618c87adf8d8a14cba69f2b622754fe2162b0
Mouads760 <159349348+Mouads760@users.noreply.github.com>
2024-02-08 17:03:30 +0100

    Update bobiz.js

--- plugins/bobiz.js
+++ plugins/bobiz.js
@@ -8,7 +8,7 @@ let handler = async (m, {
 }) => {
     await m.reply(wait)
     let res = await YesNo()
-    let stiker = await createSticker(false, res.image, "bobiza ", (res.answer).toUpperCase(), 30)
+    let stiker = await createSticker(false, res.image, "stiven ", (res.answer).toUpperCase(), 30)
         
     try {
         await m.reply(stiker)
@@ -16,9 +16,9 @@ let handler = async (m, {
         throw eror
     }
 }
-handler.help = ["bobiz"]
+handler.help = ["stiven"]
 handler.tags = ["sticker"]
-handler.command = /^(bobiz)$/i
+handler.command = /^(stive)$/i
 
 export default handler
 
