6ac846014d0e57ba80e1e1d4d1f841c1e8826348
Mouads760 <159349348+Mouads760@users.noreply.github.com>
2024-02-09 11:44:39 +0100

    Update tt.js

--- plugins/tt.js
+++ plugins/tt.js
@@ -58,7 +58,7 @@ var handler = async (m, { conn, args, usedPrefix, command }) => {
         m.chat,
         videoURL,
         "tiktok.mp4",
-        `اذا كنت تحب بوبيزة فتابع صاحبها 😄\ninstagram.com/noureddine_ouafy`,
+        'اذا كنت تحب كلاديوس فتابع صاحبه😄\ninstagram.com/stiven._.xxl`,
         m,
       );
       setTimeout(async () => {
@@ -66,7 +66,7 @@ var handler = async (m, { conn, args, usedPrefix, command }) => {
           m.chat,
           videoURLWatermark,
           "tiktokwm.mp4",
-          `اذا كنت تحب بوبيزة فتابع صاحبها 😄\ninstagram.com/noureddine_ouafy`,
+          `اذا كنت تحب كلاديوس فتابع صاحبه 😄\ninstagram.com/stiven._.xxl`,
           m,
         );
         await conn.sendFile(
@@ -78,7 +78,7 @@ var handler = async (m, { conn, args, usedPrefix, command }) => {
         );
         conn.reply(
           m.chat,
-          "اذا كنت تحب بوبيزة فتابع صاحبها 😄\ninstagram.com/noureddine_ouafy",
+          "اذا كنت تحب كلاديوس فتابع صاحبه 😄\ninstagram.com/stiven._.xxl",
           m,
         );
       }, 1500);
@@ -150,7 +150,7 @@ var handler = async (m, { conn, args, usedPrefix, command }) => {
 
       await conn.reply(
         m.chat,
-        "اذا كنت تحب بوبيزة فتابع صاحبها 😄\ninstagram.com/noureddine_ouafy",
+        "اذا كنت تحب كلاديوس فتابع صاحبه 😄\ninstagram.com/stiven._.xxl",
         m,
       );
     } catch (error2) {
