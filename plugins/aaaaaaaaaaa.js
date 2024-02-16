0611dd9136d2abd1d4b808246730b258b2f8b0c0
Youssefjo2 <159731247+Youssefjo2@users.noreply.github.com>
2024-02-17 00:28:36 +0200

    Update aaaaaaaaaaa.js

--- plugins/aaaaaaaaaaa.js
+++ plugins/aaaaaaaaaaa.js
@@ -1,4 +1,15 @@
 import fetch from 'node-fetch'
+let handler = async (m, { conn }) => {
+  let caption = `
+╭────────────────────
+│👋 مرحبا يا , ${conn.getName(m.sender)}!
+│🤖 أتمنى أنك بخير ♥ البوت أون لاين الآن 
+يمكنك إستخدامه عبر كتابة menu.
+╰────────────────────
+*─[ BY [JoAnimi] ]*🌟✨
+`.trim()
+  m.reply(caption)
+}
 const defaultMenu = {
   before: `السلام عليكم 👋. 
 
@@ -13,18 +24,19 @@ const defaultMenu = {
   body: '┃⋄ %cmd %isdiamond %isPremium',
   footer: '┗━━━━━━━━⬣\n',
   after: '*إستخدامك للبوت بشكل صحيح يعني أنك تزيد من إحتمالية أن يبقى البوت شغالا لمدة أطول . لذا إن واجهتك أي مشكلة لا تخجل من سؤال صاحب البوت .رقمه سوف تجده في الأسفل *\n+201008599375 واجه شكري ودعمي لاخي نور https://instagram.com/noureddine_ouafy',
-}
-let handler = async (m, { conn }) => {
-  let caption = `
-╭────────────────────
-│👋 مرحبا يا , ${conn.getName(m.sender)}!
-│🤖 أتمنى أنك بخير ♥ البوت أون لاين الآن 
-يمكنك إستخدامه عبر كتابة menu.
-╰────────────────────
-*─[ BY [JoAnimi] ]*🌟✨
-`.trim()
-  m.reply(caption)
-}
+  }
+ conn.sendMessage(m.chat, {
+text: text,
+contextInfo: {
+externalAdReply: {
+title: 'KILLUA BOT ♥',
+body: "بوت من تحرير الاسطوره يوسف چو 💖",
+thumbnailUrl: 'https://telegra.ph/file/e1f63555f0063389c9845.jpg',
+sourceUrl: 'https://youtube.com/@JoAnimi1?si=3y1aUCmnTVzCVgIM',
+mediaType: 1,
+renderLargerThumbnail: true
+}}}, { quoted: m})
+  
 handler.help = ['test']
 handler.tags = ['infobot']
 handler.command = /^(test)$/i
