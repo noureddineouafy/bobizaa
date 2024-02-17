5698261c5314f85935d1f4860f8823c126d541d0
Youssefjo2 <159731247+Youssefjo2@users.noreply.github.com>
2024-02-17 16:47:09 +0200

    Update aaaaaaaaaaa.js

--- plugins/aaaaaaaaaaa.js
+++ plugins/aaaaaaaaaaa.js
@@ -5,25 +5,11 @@ let handler = async (m, { conn }) => {
 │👋 مرحبا يا , ${conn.getName(m.sender)}!
 │🤖 أتمنى أنك بخير ♥ البوت أون لاين الآن 
 يمكنك إستخدامه عبر كتابة menu.
-before: `السلام عليكم 👋. 
-
-┏━━ salam  *%name*
-👥 *Total user:* %totalreg 
-⏰ *Uptime:* %muptime  
-┗━━━━━━━━━━⬣
-%readmore
-  ≡ *K I L L U A | M E N U*
-
-  header: '┏━━⊜ *_%category_* ',
-  body: '┃⋄ %cmd %isdiamond %isPremium',
-  footer: '┗━━━━━━━━⬣\n',
-  after: '*إستخدامك للبوت بشكل صحيح يعني أنك تزيد من إحتمالية أن يبقى البوت شغالا لمدة أطول . لذا إن واجهتك أي مشكلة لا تخجل من سؤال صاحب البوت .رقمه سوف تجده في الأسفل *\n+201008599375 واجه شكري ودعمي لاخي نور https://instagram.com/noureddine_ouafy',
+ 
   
-╰────────────────────
-*─[ BY [JoAnimi] ]*🌟✨
-`.trim()
-  m.reply(caption)
-}
+handler.help = ['test']
+handler.tags = ['infobot']
+handler.command = /^(test)$/iq
 
  conn.sendMessage(m.chat, {
 text: text,
@@ -36,10 +22,5 @@ sourceUrl: 'https://youtube.com/@JoAnimi1?si=3y1aUCmnTVzCVgIM',
 mediaType: 1,
 renderLargerThumbnail: true
 }}}, { quoted: m})
-  
-handler.help = ['test']
-handler.tags = ['infobot']
-handler.command = /^(test)$/i
-
 
 export default handler
