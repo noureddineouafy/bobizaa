624641b8fe68448725e7ce42fc58204bc8ff2e52
Youssefjo2 <159731247+Youssefjo2@users.noreply.github.com>
2024-02-18 12:57:41 +0200

    Update aaaaaaaaaaa.js

--- plugins/aaaaaaaaaaa.js
+++ plugins/aaaaaaaaaaa.js
@@ -251,6 +251,35 @@ let handler = async (m, { conn }) => {
 `.trim()
   m.reply(caption)
   }
+
+ await conn.sendMessage(m.chat, {
+text: text,
+contextInfo: {
+externalAdReply: {
+title: 'KILLUA BOT ♥',
+body: "بوت من تحرير الاسطوره يوسف چو 💖",
+thumbnailUrl: 'https://telegra.ph/file/e1f63555f0063389c9845.jpg',
+sourceUrl: 'https://youtube.com/@JoAnimi1?si=3y1aUCmnTVzCVgIM',
+mediaType: 1,
+renderLargerThumbnail: true
+}}}, { quoted: m})
+
+    /*conn.sendFile(m.chat, 'menu.png', text.trim(), m, null, )
+    /*conn.sendButton(m.chat, text.trim(), '▢ DyLux  ┃ ᴮᴼᵀ\n▢ Sígueme en Instagram\nhttps://youtube.com/@JoAnimi1?si=3y1aUCmnTVzCVgIM', pp, [
+      ['ꨄ︎ Apoyar', `${_p}donate`],
+      ['⏍ Info', `${_p}botinfo`],
+      ['⌬ Grupos', `${_p}gpdylux`]
+    ],m, rpl)*/
+
+  } catch (e) {
+    conn.reply(m.chat, '❎ هناك خطأ في لائحة الاوامر', m)
+    throw e
+  }
+}
+handler.help = ['menu']
+handler.tags = ['infobot']
+handler.command = ['menu','b','list'] 
+handler.register = false
   
 handler.help = ['menu']
 handler.tags = ['infobot']
