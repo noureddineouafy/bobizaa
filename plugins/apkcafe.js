4bf2bc265cf0bf4c97b323ece66701cd66250763
Botbilal <101138466+Botbilal@users.noreply.github.com>
2024-02-24 14:01:39 +0100

    Update apkcafe.js

--- plugins/apkcafe.js
+++ plugins/apkcafe.js
@@ -1,174 +1,32 @@
-import cheerio from 'cheerio';
 import fetch from 'node-fetch';
+const handler = (m) => m;
 
-let handler = async (m, {
-    conn,
-    args,
-    usedPrefix,
-    text,
-    command
-}) => {
-
-    let lister = [
-        "s",
-        "d"
-    ]
-
-    let [feature, inputs, inputs_, inputs__, inputs___] = text.split("+")
-    if (!lister.includes(feature)) return m.reply("*هذا الامر خاص بتحميل التطبيقات من موقع \n https://apk.cafe/\nيمكنك تحميل التطبيقات من هذا الامر من خلال كتابة\n*.apkcafe s+instagram lite*\nبعد ان تحصل على رابط التطبيق تعود للبوت وتكتب له هذا الامر لتحميله\n*.apkcafe d+*(رابط التطبيق) \n\n\n*options*\n" + lister.map((v, index) => "  ○ " + v).join("\n"))
-
-    if (lister.includes(feature)) {
-
-        if (feature == "s") {
-            if (!inputs) return m.reply("مثال:\n.apkcafe s+instagram lite")
-            await m.reply(wait)
-            try {
-                let res = await searchApp(inputs)
-                let teks = res.map((item, index) => {
-                    return `🔍 *[ RESULT ${index + 1} ]*
-
-📢 *title:* ${item.name}
-🌐 *url:* ${item.href}
-🖼️ *image:* ${item.imgUrl}
-🔖 *name:* ${item.rightHref}
-`
-
-                }).filter(v => v).join("\n\n________________________\n\n")
-                await m.reply(teks)
-            } catch (e) {
-                await m.reply('error')
-            }
-        }
-
-        if (feature == "d") {
-            if (!inputs) return m.reply("مثال \n .apkcafe d+https://instagram-lite.apk.cafe")
-            await m.reply(wait)
-            try {
-                let obje = await getInfo(inputs)
-                let item = obje.version[0]
-                let cap = `🔍 *[ RESULT ]*
-
-📌 *title:* ${item.title}
-🖼️ *image:* ${item.image}
-🔗 *url:* ${item.url}
-⬇️ *downloadLink:* ${item.downloadLink}
-📦 *fileSize:* ${item.fileSize}
-📱 *deviceInfo:* ${item.deviceInfo}
-🤖 *androidVersion:* ${item.androidVersion}
-💾 *downloadText:* ${item.download.downloadText}
-🔗 *directLink:* ${item.download.directLink}
-📋 *apkTechnicalInfo:*
-${Object.entries(item.download.apkTechnicalInfo)
-  .map(([key, value]) => { return key + value.trim() })
-  .join('\n')}
-`
-                await conn.sendFile(m.chat, item.image || logo, "", cap, m)
-                await conn.sendFile(m.chat, item.download.directLink || logo, item.title || 'Tidak diketahui', null, m, true, {
-                    quoted: m,
-                    mimetype: "application/vnd.android.package-archive"
-                })
-            } catch (e) {
-                await m.reply('error')
-            }
-        }
+handler.before = async (m) => {
+  const chat = global.db.data.chats[m.chat];
+  if (chat.simi) {
+    if (/^.*false|disnable|(turn)?off|0/i.test(m.text)) return;
+    let textodem = m.text;
+    try {
+      const ressimi = await fetch(`https://api.simsimi.net/v2/?text=${encodeURIComponent(textodem)}&lc=es`);
+      const data = await ressimi.json();
+      if (data.success == 'No s\u00e9 lo qu\u00e9 est\u00e1s diciendo. Por favor ense\u00f1ame.') return m.reply(`${lol}`); /* EL TEXTO "lol" NO ESTA DEFINIDO PARA DAR ERROR Y USAR LA OTRA API */
+      await m.reply(data.success);
+    } catch {
+      /* SI DA ERROR USARA ESTA OTRA OPCION DE API DE IA QUE RECUERDA EL NOMBRE DE LA PERSONA */
+      if (textodem.includes('Hola')) textodem = textodem.replace('Hola', 'Hello');
+      if (textodem.includes('hola')) textodem = textodem.replace('hola', 'hello');
+      if (textodem.includes('HOLA')) textodem = textodem.replace('HOLA', 'HELLO');
+      const reis = await fetch('https://translate.googleapis.com/translate_a/single?client=gtx&sl=auto&tl=en&dt=t&q=' + textodem);
+      const resu = await reis.json();
+      const nama = m.pushName || '1';
+      const api = await fetch('http://api.brainshop.ai/get?bid=153868&key=rcKonOgrUFmn5usX&uid=' + nama + '&msg=' + resu[0][0][0]);
+      const res = await api.json();
+      const reis2 = await fetch('https://translate.googleapis.com/translate_a/single?client=gtx&sl=auto&tl=es&dt=t&q=' + res.cnt);
+      const resu2 = await reis2.json();
+      await m.reply(resu2[0][0][0]);
     }
-}
-handler.help = ["apkcafe"]
-handler.tags = ["applications"]
-handler.command = /^(apkcafe)$/i
-handler.premium = false
-export default handler
-
-/* New Line */
-async function searchApp(query) {
-    const url = `https://apk.cafe/ajax/apk-search.php?s=${query}`;
-    const response = await fetch(url);
-    const html = await response.text();
-
-    const $ = cheerio.load(html);
-    const results = [];
-
-    $('.sugg_row').each((index, element) => {
-        const suggImg = $(element).find('.sugg_img');
-        const suggText = $(element).find('.sugg_text');
-        const suggRight = $(element).find('.sugg_right');
-
-        const app = {
-            href: suggImg.attr('href'),
-            imgUrl: suggImg.find('img').attr('src'),
-            name: suggText.text(),
-            rightHref: suggRight.attr('href')
-        };
-
-        results.push(app);
-    });
-
-    return results;
-}
-
-async function getInfo(url) {
-    const response = await fetch(url);
-    const html = await response.text();
-
-    const $ = cheerio.load(html);
-
-    const appDetails = [];
-
-    const promises = $('li.dwn_up').map(async (index, element) => {
-        const downloadLink = $(element).find('a.dwn1').attr('href');
-        if (downloadLink.includes('https://apk.cafe/download?file_id')) {
-            const fileSize = $(element).find('a.dwn1 span').text().trim();
-            const deviceInfo = $(element).find('div.additional_file_info b').text().trim();
-            const androidVersion = $(element).find('div.additional_file_info').find('.f_ifo:last-child').text().trim();
-
-            const download = await getDetails(downloadLink);
-
-            const app = {
-                title: $('meta[property="og:title"]').attr('content'),
-                image: $('meta[property="og:image"]').attr('content'),
-                url: $('meta[property="og:url"]').attr('content'),
-                downloadLink,
-                fileSize,
-                deviceInfo,
-                androidVersion,
-                download
-            };
-
-            appDetails.push(app);
-        }
-    }).get();
-
-    await Promise.all(promises);
-
-    return {
-        version: appDetails
-    };
-}
-
-async function getDetails(url) {
-    const response = await fetch(url);
-    const html = await response.text();
-
-    const $ = cheerio.load(html);
-
-    const appDetails = {};
-
-    const downloadText = $('.text_up2 .text_up').text().trim();
-    const directLink = $('.text_up2 .download_text a.dwnDirect').attr('href');
-
-    appDetails.downloadText = downloadText;
-    appDetails.directLink = directLink;
-
-    const apkTechnicalInfo = {};
-
-    $('.dwn_params_wrap .dwn_params li').each((index, element) => {
-        const key = $(element).find('b').text().trim().replace(':', '');
-        const value = $(element).text().replace(key, '').trim();
-
-        apkTechnicalInfo[key] = value;
-    });
-
-    appDetails.apkTechnicalInfo = apkTechnicalInfo;
-
-    return appDetails;
-}
+    return !0;
+  }
+  return true;
+};
+export default handler;
