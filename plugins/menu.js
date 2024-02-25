faf8ea64d11595f23caaf86ec113671bf45addce
Yonass12 <157059976+Yonass12@users.noreply.github.com>
2024-02-25 12:29:53 +0100

    Update menu.js

--- plugins/menu.js
+++ plugins/menu.js
@@ -173,4 +173,4 @@ function clockString(ms) {
   let m = isNaN(ms) ? '--' : Math.floor(ms / 60000) % 60
   let s = isNaN(ms) ? '--' : Math.floor(ms / 1000) % 60
   return [d, 'd ', h, 'h ', m, 'm '].map(v => v.toString().padStart(2, 0)).join('')
-    }
+      }
