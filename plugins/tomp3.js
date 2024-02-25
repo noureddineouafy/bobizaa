661f1758903cf6a6391e26b751a2194a1c8a867d
Yonass12 <157059976+Yonass12@users.noreply.github.com>
2024-02-25 18:04:54 +0100

    Update tomp3.js

--- plugins/tomp3.js
+++ plugins/tomp3.js
@@ -1,16 +1,29 @@
-import {toAudio} from '../lib/converter.js';
-const handler = async (m, {conn, usedPrefix, command}) => {
-  const q = m.quoted ? m.quoted : m;
-  const mime = (q || q.msg).mimetype || q.mediaType || '';
-  if (!/video|audio/.test(mime)) throw `*هذا الامر خاص بتحويل مقطع فيديو لموسيقى*`;
-  const media = await q.download();
-  if (!media) throw '*تأكد انك قمت بارسال فيديو وتأكد انك كتبت \n.tomp3*';
-  const audio = await toAudio(media, 'mp4');
-  if (!audio.data) throw '*حذث خطأ راسل صاحب البوت*';
-  conn.sendMessage(m.chat, {audio: audio.data, mimetype: 'audio/mpeg'}, {quoted: m});
-};
-;
-handler.help = ["tomp3"]
-handler.tags = ["tools"]
-handler.command = /^(tomp3)$/i
-export default handler;
+Module_Exports({
+kingcmd: "tomp3",
+shortcut:['mp3','toaudio'],
+infocmd: "changes type to audio.",
+kingclass: "converter",
+use: 'reply to any Video',
+kingpath: __filename
+},
+async(sigma, citel, text) => {
+if (!citel.quoted) return citel.reply(`_Reply to Any Video_`);
+let mime = citel.quoted.mtype
+if (mime =="audioMessage" || mime =="videoMessage")
+{
+let media = await sigma.downloadAndSaveMediaMessage(citel.quoted);
+const { toAudio } = require('../lib');
+let buffer = fs.readFileSync(media);
+let audio = await toAudio(buffer);
+sigma.sendMessage(citel.chat, { audio: audio, mimetype: 'audio/mpeg' }, { quoted: citel });
+
+
+fs.unlink(media, (err) => {
+if (err) { return console.error('File Not Deleted from From TOAUDIO AT : ' , media,'\n while Error : ' , err); }
+else return console.log('File deleted successfully in TOAUDIO MP3 at : ' , media);
+});
+
+}
+else return citel.send ("*Please, Reply To A video Message*")
+}
+)
