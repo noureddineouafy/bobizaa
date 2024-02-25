f9444812e01d4d6c8d4155b079716118921953bb
Youssefjo2 <159731247+Youssefjo2@users.noreply.github.com>
2024-02-26 01:41:33 +0200

    Update alive.js

--- plugins/alive.js
+++ plugins/alive.js
@@ -11,34 +11,10 @@ let handler = async (m, { conn }) => {
   m.reply(caption)
 }
 
- await conn.sendMessage(m.chat, {
-text: text,
-contextInfo: {
-externalAdReply: {
-title: 'KILLUA BOT ♥',
-body: "بوت من تحرير الاسطوره يوسف چو 💖",
-thumbnailUrl: 'https://telegra.ph/file/e1f63555f0063389c9845.jpg',
-sourceUrl: 'https://youtube.com/@JoAnimi1?si=3y1aUCmnTVzCVgIM',
-mediaType: 1,
-renderLargerThumbnail: true
-}}}, { quoted: m})
 
-    /*conn.sendFile(m.chat, 'menu.png', text.trim(), m, null, )
-    /*conn.sendButton(m.chat, text.trim(), '▢ DyLux  ┃ ᴮᴼᵀ\n▢ Sígueme en Instagram\nhttps://youtube.com/@JoAnimi1?si=3y1aUCmnTVzCVgIM', pp, [
-      ['ꨄ︎ Apoyar', `${_p}donate`],
-      ['⏍ Info', `${_p}botinfo`],
-      ['⌬ Grupos', `${_p}gpdylux`]
-    ],m, rpl)*/
-
-  } catch (e) {
-    conn.reply(m.chat, '❎ هناك خطأ في لائحة الاوامر', m)
-    throw e
-  }
-}
-
-handler.help = ['m']
+handler.help = ['alive']
 handler.tags = ['infobot']
-handler.command = /^(m)$/i
+handler.command = /^(alive)$/i
 
 
 export default handler
