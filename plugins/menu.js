63ddd23aa1e540d82556b94c011c69667fc976f2
Botbilal <101138466+Botbilal@users.noreply.github.com>
2024-02-26 21:36:43 +0100

    Update menu.js

--- plugins/menu.js
+++ plugins/menu.js
@@ -143,7 +143,7 @@ sourceUrl: 'https://instagram.com/noureddine_ouafy',
 mediaType: 1,
 renderLargerThumbnail: true
 }}}, { quoted: m})
-conn.sendMessage(m.chat, {audio: { url: 'https://files.catbox.moe/dtb3rq.mp3'}, mimetype:'audio/mpeg', ptt:true }, {quoted:m})
+conn.sendMessage(m.chat, {audio: { url: 'https://files.catbox.moe/7dcwb9.mp3'}, mimetype:'audio/mpeg', ptt:true }, {quoted:m})
     /*conn.sendFile(m.chat, 'menu.png', text.trim(), m, null, )
     /*conn.sendButton(m.chat, text.trim(), '▢ DyLux  ┃ ᴮᴼᵀ\n▢ Sígueme en Instagram\nhttps://www.instagram.com/fg98_ff', pp, [
       ['ꨄ︎ Apoyar', `${_p}donate`],
