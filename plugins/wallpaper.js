f509467b4f1a89f87b9e9a54a6052d871112971b
Botbilal <101138466+Botbilal@users.noreply.github.com>
2024-04-10 07:53:20 +0000

    Update wallpaper.js

--- plugins/wallpaper.js
+++ plugins/wallpaper.js
@@ -2,12 +2,12 @@ import { wallpaper } from "../lib/scrape.js";
 
 let handler = async (m, { conn, command, text, usedPrefix }) => {
     let query = text
-    if (!query) throw `مثال: \n\n*.wallpaper girl*`;
+    if (!query) throw `مثال: \n\n*.wallpaper cat*`;
     m.reply(wait);
     try {
         let wallpapers = await wallpaper(query);
         let randomWallpaper = wallpapers[Math.floor(Math.random() * wallpapers.length)];
-        let cap = "تابع مالك البوت في حسابه\ninstagram.com/noureddine_ouafy";
+        let cap = "تابع مالك البوت في حسابه\ninstagram.com/127.0.0.1_8888";
         conn.sendMessage(m.chat, { image: { url: randomWallpaper }, caption: cap }, m);
     } catch (e) {
         console.log(e);
