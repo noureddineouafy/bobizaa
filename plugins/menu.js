ca08ccb2b46cdf71b3ac9d9603dd8dbbe5d02f70
KSoufian <168342075+KSoufian@users.noreply.github.com>
2024-04-28 18:31:17 +0100

    Update menu.js

--- plugins/menu.js
+++ plugins/menu.js
@@ -4,7 +4,7 @@ import fetch from 'node-fetch'
 import { xpRange } from '../lib/levelling.js'
 //import { plugins } from '../lib/plugins.js'
 let tags = {
-
+  'morocco':'  ‎أوامر للمغاربة',
   'applications':'‎ أوامر التطبيقات‎',
   'drawing':'‎ توليد الصور‎ أوامر' ,
   'ai':'‎ الذكاء الاصطناعي‎ أوامر',
@@ -14,7 +14,7 @@ let tags = {
   'islam':'‎ الدين هو الاسلام‎',
   'owner':'‎ اوامر صاحب البوت',
   'search':'‎ أوامر البحث',
-  'audio-changer':'‎ تعديل 79j',
+  'audio-changer':'‎ تعديل الصوتيات‎',
   'sticker':'‎ أوامر الملصقات',
   'image-edit':'‎ تعديل الصور',
   'pdf':'‎ pdf ومشتقاته‎',
