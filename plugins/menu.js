3b8d6ab220ce225f51c7832d8c3bdf3c067080ba
KSoufian <168342075+KSoufian@users.noreply.github.com>
2024-06-01 21:41:52 +0200

    Update menu.js

--- plugins/menu.js
+++ plugins/menu.js
@@ -7,7 +7,7 @@ import { promises } from 'fs'
 import { join } from 'path'
 let handler = async (m, { conn, usedPrefix, usedPrefix: _p, __dirname, text, isPrems }) => {
 try {
-let vn = './Menu4.png'
+let vn = './Menu4.mp4'
 let pp = imagen4
 let img = await(await fetch('https://telegra.ph/.')).buffer()
 let d = new Date(new Date + 3600000)
